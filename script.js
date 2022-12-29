87fa0d2520132c5d4c74dc30e481a6bf333e4728
prudhvi1601 <prudhvi1601@gmail.com>
2022-12-29 23:14:10 +0530

    bs carousel

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your code here
+ 
+ $('.owl-carousel').owlCarousel({
+    items:5,
+    loop: true,
+    nav: false,
+    autplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    respnsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
